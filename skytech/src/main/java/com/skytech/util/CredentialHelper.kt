f7a7751cfb690f2bc17181ba7a574ddd917b43c3
yunisrasulzade <yunisrasulzada99@gmail.com>
2022-10-11 14:31:44 +0400

    endless loading bu fix

--- skytech/src/main/java/com/skytech/util/CredentialHelper.kt
+++ skytech/src/main/java/com/skytech/util/CredentialHelper.kt
@@ -1,8 +1,8 @@
 package com.skytech.util
 
+import androidx.lifecycle.MutableLiveData
 import com.skytech.model.HashCredential
 
 object CredentialHelper {
-    val credential = SingleLiveEvent<HashCredential>()
+    val credential = MutableLiveData<HashCredential>()
 }
-
